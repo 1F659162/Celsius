401bc7aa2c6c365f7eb44c6bc45843372fee7e95
1F659162 <45314100+1F659162@users.noreply.github.com>
2019-07-03 13:13:04 +0700

    Add files via upload

---
+++ Celsius.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	float celsius ;
+	float fahrenheit;
+
+	cout << " Enter celsius temperater : " ;
+	cin >> celsius ;
+	fahrenheit = celsius * 3.4;
+	cout << " Fahrenheit = " << fahrenheit << endl;
+	cout << " Now weather in thailand is " << ( fahrenheit > 70 ? " HOT " : " Cool \n") ;
+
+	return(0);
+}
